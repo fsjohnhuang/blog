9e0d894aea11b9711536d77ccf2b68ea70459862
^_^肥仔John <fsjohnhuang@hotmail.com>
2023-12-19 18:20:01 +0800

    chore: update next.js config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withMDX = require('@next/mdx')()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'out',
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
